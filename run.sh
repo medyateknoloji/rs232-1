2fa75c9c7ef32e16683d1ff09a9fc790d60e791c
tayfunerkorkmaz <tayfunerkorkmaz@gmail.com>
2017-11-08 14:11:51 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-echo "Guncelleme basliyor..."
 sudo rm /boot/cmdline.txt
-echo "cmdline.txt silindi"
 sudo touch /boot/cmdline.txt
-echo "cmdline.txt olusturuldu"
 sudo sh -c "echo 'consoleblank=1 logo.nologo dwc_otg.lpm_enable=0 console=tty1 root=PARTUUID=84fa8189-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait  quiet   plymouth.ignore-serial-consoles loglevel=1 quiet' >> /boot/cmdline.txt"
-echo "pyserial kutuphanesi kuruluyor..."
 sudo python -m pip install pyserial
 cp /home/pi/piSignagePro/rs232/TVon.py /home/pi/TVon.py
 cp /home/pi/piSignagePro/rs232/TVoff.py /home/pi/TVoff.py
-echo "Dosyalar kopyalandi..."
-echo "crontab ekleniyor..."
 touch mycron
 echo "# m h  dom mon dow   command" >> mycron
 echo "@reboot python /home/pi/TVon.py" >> mycron
 echo "00 04 * * * sudo reboot" >> mycron
 crontab mycron
 rm mycron
+sudo rm /etc/wpa_supplicant/wpa_supplicant.conf
+sudo cp /home/pi/piSignagePro/cpwifi/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
+echo "Islem Tamamlandi."
